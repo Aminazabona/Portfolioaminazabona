225a70ae49f32630f01cd6749f89f2ddc1bdfce5
Amina Zabona <aminazabona8@gmail.com>
2026-09-17 14:46:18 +0200

    feat:fix certifiats sections

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -1,3 +1,7 @@
+import certificatTalent from "../assets/images/aminazabonacertificat.jpeg";
+import certificatIA from "../assets/images/aminazabonaesther.jpeg";
+import photoAmina from "../assets/images/aminadefense.jpg";
+
 export const certificates = [
   {
     id: "certif-talent4startup",
@@ -5,37 +9,39 @@ export const certificates = [
     issuer: "Afrix Global – Talent4Startup",
     year: "2026",
     credentialId: "AFX-T4S-2026",
-    image: "/src/assets/images/AMINA_ZABONA_ESTHER1CERTIFICAT.jpeg",
-    photoUrl: "/src/assets/images/AMINA_ZABONA_ESTHER1CERTIFICAT.jpeg",
-    documentUrl: "/src/assets/images/AMINA_ZABONA_ESTHER1CERTIFICAT.jpeg",
+    image: certificatTalent,
+    photoUrl: certificatTalent,
+    documentUrl: certificatTalent,
     badgeLabel: "Fullstack",
     buttonText: "Voir le certificat",
     isDocument: false,
-    imageLabel: "/src/assets/images/AMINA_ZABONA_ESTHER1CERTIFICAT.jpeg",
+    imageLabel: "Certificat Développeuse Web Fullstack",
   },
+
   {
     id: "certif-ia-goinnovation",
     title: "Certificat en Intelligence Artificielle",
     issuer: "Go Innovation — Programme Go Skills",
     year: "2026",
     credentialId: "GO-AI-2026",
-    image: "/src/assets/images/AMINA_ZABONA_CERTIFICATS.jpeg",
-    photoUrl: "/src/assets/images/AMINA_ZABONA_CERTIFICATS.jpeg",
-    documentUrl: "/src/assets/images/AMINA_ZABONA_CERTIFICATS.jpeg",
+    image: certificatIA,
+    photoUrl: certificatIA,
+    documentUrl: certificatIA,
     badgeLabel: "IA & Data",
     buttonText: "Voir le certificat",
     isDocument: false,
     imageLabel: "Photo du certificat IA",
   },
+
   {
     id: "certif-isig",
     title: "Licence en Informatique Appliquée à la Gestion des entreprises",
     issuer: "ISIG – Goma",
     year: "2023-2026",
     credentialId: "ISIG-L3-2026",
-    image: "/src/assets/images/FMP FRED(16).jpg",
-    photoUrl: "/src/assets/images/FMP FRED(16).jpg",
-    documentUrl: "/src/assets/documents/RELEVER  DES COTES.pdf",
+    image: photoAmina,
+    photoUrl: photoAmina,
+    documentUrl: "/documents/RELEVER  DES COTES.pdf",
     badgeLabel: "Relevés",
     buttonText: "Voir mes relevés",
     isDocument: true,
